18ca51e6373c527d8a6d96121a228b1860079aa7
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-08-29 22:26:56 -0700

    Add logfrog tests for LFQ and capture

--- python/logfrog/__init__.py
+++ python/logfrog/__init__.py
@@ -1,4 +1,25 @@
 # from typing import Final
+# from .logfrog import LFQueue
+import time
+
+# TODO: Define
+# LFQueue will be a singleton
+# LFQ = LFQueue()
+
+
+def capture(func):
+    def wrapper():
+        start = time.time()
+
+        func()
+
+        end = time.time()
+
+        # TODO: Internally add start and end to LogFrog queue
+        # LFQ.add(start, end)
+
+    return wrapper
+
 
 def decorator_example(func):
     def wrapper():
@@ -10,6 +31,7 @@ def decorator_example(func):
         print("LogFrog end...")
 
     return wrapper
+
 
 # Use when Rust is added
 # __all__: Final[list[str]] = [
--- python/tests/test_init.py
+++ python/tests/test_init.py
@@ -13,3 +13,21 @@ def test_wrapper(capsys):
     stdout = capsys.readouterr()
 
     assert stdout.out == "LogFrog start...\nLogFrog end...\n"
+
+
+def test_capture():
+    from logfrog import capture
+
+    # from .logfrog import LFQueue
+    # LFQ = LFQueue()
+
+    @capture
+    def foo(): pass
+
+    # LFQ.drop()
+
+    # assert LFQ.empty()
+
+    foo()
+
+    # assert not LFQ.empty()
