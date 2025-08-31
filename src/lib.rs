0a0589b9464779c81c5d31a8c98d7d4fdbe3b36a
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-08-31 12:47:10 -0700

    Add py03 specific stuff and switch to epoch

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
 use pyo3::prelude::*;
 use std::collections::VecDeque;
-use std::time::Instant;
+
+type Epoch = u64;
 
 struct Capture {
-    start: Instant,
-    end: Instant,
+    start: Epoch,
+    end: Epoch,
 }
 
+#[pyclass]
 struct LFQueue {
     queue: VecDeque<Capture>,
 }
 
+#[pymethods]
 impl LFQueue {
-    pub fn capture(&mut self, start: Instant, end: Instant) {
+    pub fn capture(&mut self, start: Epoch, end: Epoch) {
         let c = Capture { start, end };
         self.queue.push_back(c);
     }
 
+    #[new]
     pub fn new() -> Self {
         LFQueue {
             queue: VecDeque::new(),
@@ -28,35 +32,35 @@ impl LFQueue {
     }
 }
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn logfrog(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<LFQueue>()?;
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Duration;
+    use std::time::{SystemTime, UNIX_EPOCH};
 
     #[test]
     fn add_to_lfq_test() {
         let mut lfq = LFQueue::new();
 
-        let t1 = Instant::now();
-        let t2 = Instant::now() + Duration::from_millis(100);
+        let t1 = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Not outatime.")
+            .as_secs();
 
+        let t2 = t1 + 100;
+
+        // Check that it's empty before we add
         assert!(lfq.empty());
 
         lfq.capture(t1, t2);
 
+        // Check that it's not empty after we add
         assert!(!lfq.empty());
     }
 }
