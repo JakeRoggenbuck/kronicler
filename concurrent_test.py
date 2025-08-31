7dd848151fab69f3fbf5b00b7a9b7180cd40726d
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-08-31 16:32:13 -0700

    Remove comment

---
+++ concurrent_test.py
@@ -0,0 +1,22 @@
+import threading
+import logfrog
+import time
+
+@logfrog.capture
+def worker(name):
+    print(f"{name} starting...")
+    time.sleep(2)
+    print(f"{name} finished!")
+
+threads = []
+
+for x in range(20):
+    t = threading.Thread(target=worker, args=(f"Thread {x}",))
+
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
+
+print("All threads done!")
