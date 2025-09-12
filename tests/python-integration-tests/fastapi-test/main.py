35bad5269cd3805af16d83cd841944d687ffd854
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-09-12 14:26:24 -0700

    Add logs rough

--- tests/python-integration-tests/fastapi-test/main.py
+++ tests/python-integration-tests/fastapi-test/main.py
@@ -3,6 +3,7 @@ import uvicorn
 import kronicler
 
 app = FastAPI()
+db = kronicler.Database()
 
 
 # You need to wrap helper functions
@@ -10,10 +11,17 @@ app = FastAPI()
 def foo():
     return {"Hello": "World"}
 
+
 # You cannot wrap routes right now
 @app.get("/")
 def read_root():
     return foo()
+
+
+@app.get("/logs")
+def read_logs():
+    return db.fetch_all()
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
