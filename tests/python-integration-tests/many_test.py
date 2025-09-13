b4ddacb31426bdf737b7f3f895bb7597b77bb7b1
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-09-13 14:21:48 -0700

    Fix tests

--- tests/python-integration-tests/many_test.py
+++ tests/python-integration-tests/many_test.py
@@ -1,5 +1,8 @@
 import kronicler
 
+# If you want a sync consume database
+kronicler.DB = kronicler.Database(sync_consume=True)
+
 
 @kronicler.capture
 def first_function():
--- tests/python-integration-tests/simple_sync_test.py
+++ tests/python-integration-tests/simple_sync_test.py
@@ -1,7 +1,7 @@
 import kronicler
 
 # If you want a sync consume database
-kronicler.DB = kronicler.Database(True)
+kronicler.DB = kronicler.Database(sync_consume=True)
 
 @kronicler.capture
 def foo():
