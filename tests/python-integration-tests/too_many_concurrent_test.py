393595d1a7d305721cb2312646926dce59e46f02
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-09-13 14:24:08 -0700

    Add concurrent version

--- tests/python-integration-tests/too_many_concurrent_test.py
+++ tests/python-integration-tests/too_many_concurrent_test.py
@@ -1,7 +1,7 @@
 import kronicler
 
 
-kronicler.DB = kronicler.Database(sync_consume=True)
+kronicler.database_init()
 
 
 @kronicler.capture
@@ -11,3 +11,7 @@ def first_function():
 if __name__ == "__main__":
     for i in range(1000):
         first_function()
+
+# TODO: Make a kronicler.cleanup method that calls the consume one last time.
+import time
+time.sleep(1)
