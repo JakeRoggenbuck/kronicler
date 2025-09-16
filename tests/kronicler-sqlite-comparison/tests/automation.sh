fcbf607d87de8c4f32cfe7af7d1df1570eb74422
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-09-15 19:59:31 -0700

    Fix automation

--- tests/kronicler-sqlite-comparison/tests/automation.sh
+++ tests/kronicler-sqlite-comparison/tests/automation.sh
@@ -1,12 +1,13 @@
-p ./simple_sync.py
+#!/bin/sh
+python3 ./simple_sync.py
 git add -u
 git commit -m "automated checkpoint after simple_sync"
 ./clean.sh
 
-p ./simple_concurrent.py
+python3 ./simple_concurrent.py
 git add -u
 git commit -m "automated checkpoint after simple_concurrent"
 ./clean.sh
 
-p ./graph_sync.py
-p ./graph_concurrent.py
+python3 ./graph_sync.py
+python3 ./graph_concurrent.py
