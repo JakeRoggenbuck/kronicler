12e9d74b49bb2b7976b062fb0b87ceb33c2552ad
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-10-05 14:56:37 -0700

    Add comments

--- tests/python-integration-tests/run_large_avg.sh
+++ tests/python-integration-tests/run_large_avg.sh
@@ -2,6 +2,12 @@ rm -rf .kronicler_data
 
 python3 ./large_average.py
 
+echo "Finished running large_average.\n\n"
+
 rm -rf .kronicler_data
 
 python3 ./large_average_no_in_mem_comp.py
+
+echo "Finished running large_average_no_in_mem_comp.\n\n"
+
+echo "Note: large_average has a higher insert time because it needs to store ground truth values in memory. The ground truth average will also be quicker because it uses only in-memory values."
